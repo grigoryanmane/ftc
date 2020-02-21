330a53e8b93baf49412d2be9ef965c59632da9cc
grigoryanmane <manegrigoryan1995@gmail.com>
2020-02-22 01:00:22 +0400

    REQUEST for edit user

---
+++ src/main/java/aca/project/ftc/model/request/UserEditRequest.java
@@ -0,0 +1,23 @@
+package aca.project.ftc.model.request;
+
+import aca.project.ftc.model.constants.Gender;
+import aca.project.ftc.model.constants.Region;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class UserEditRequest {
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private Region region;
+}
