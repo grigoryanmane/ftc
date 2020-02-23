c02a527f90dc4558ff60b4ae06f65bb428d6024d
grigoryanmane <manegrigoryan1995@gmail.com>
2020-02-23 16:40:10 +0400

    Region with keys and updated values

--- src/main/java/aca/project/ftc/model/constants/Region.java
+++ src/main/java/aca/project/ftc/model/constants/Region.java
@@ -1,5 +1,29 @@
 package aca.project.ftc.model.constants;
 
 public enum Region {
-    YEREVAN, KOTAYQ, VAYOTSDZOR, TAVUSH, SYUNIQ, SHIRAK, LORI, GEGHARQUNIK, ARMAVIR, ARARAT, ARAGATSOTN
+
+    ARAGATSOTN("Aragatsotn"),
+    ARARAT("Ararat"),
+    ARMAVIR("Armavir"),
+    GEGHARQUNIQ("Gegharquniq"),
+    //TODO::CHAGE LORY TO LORI FROM FRONT ENT
+    LORI("Lori"),
+    KOTAYQ("Kotayq"),
+    SHIRAK("Shirak"),
+    SYUNIQ("Syunik"),
+    VAYOTSDZOR("Vayots Dzor"),
+    TAVUSH("Tavush"),
+    YEREVAN("Yerevan");
+
+    private final String key;
+
+    Region(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+
 }
