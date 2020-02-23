40275c0325363e3645068e756dbba283bf19a15a
grigoryanmane <manegrigoryan1995@gmail.com>
2020-02-23 16:39:40 +0400

    Gender with keys

--- src/main/java/aca/project/ftc/model/constants/Gender.java
+++ src/main/java/aca/project/ftc/model/constants/Gender.java
@@ -1,5 +1,15 @@
 package aca.project.ftc.model.constants;
 
 public enum Gender {
-    FEMALE, MALE, OTHER
+    FEMALE("Female"), MALE("Male"), OTHER("Other");
+
+    private final String key;
+
+    Gender(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
 }
