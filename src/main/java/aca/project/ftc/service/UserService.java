b56a2a143ba8d06edcbe35d0998d896b6fb383d6
grigoryanmane <manegrigoryan1995@gmail.com>
2020-02-24 12:58:43 +0400

    User controller for edit user

--- src/main/java/aca/project/ftc/service/UserService.java
+++ src/main/java/aca/project/ftc/service/UserService.java
@@ -1,26 +1,18 @@
 package aca.project.ftc.service;
 
 
-import aca.project.ftc.model.UserModel;
 import aca.project.ftc.model.UserProductModel;
-import aca.project.ftc.model.request.DeleteUserRequest;
-import aca.project.ftc.model.request.UserEditRequest;
 import aca.project.ftc.model.request.UserProductRequest;
-import aca.project.ftc.model.response.User;
-import aca.project.ftc.model.response.UserEditResponseDto;
 import aca.project.ftc.model.response.UserProductResponseDto;
 import aca.project.ftc.repository.ProductRepository;
 import aca.project.ftc.repository.UserProductRepository;
 import aca.project.ftc.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -50,20 +42,6 @@ public class UserService {
         }
     }
 
-
-    public UserProductResponseDto setUserProductDto(UserProductModel userProductModel) {
-        UserProductResponseDto userProductResponseDto = new UserProductResponseDto();
-        userProductResponseDto.setId(userProductModel.getId());
-        userProductResponseDto.setUserId(userProductModel.getUser().getId());
-        userProductResponseDto.setProductId(userProductModel.getProduct().getId());
-        userProductResponseDto.setAmount(userProductModel.getAmount());
-        userProductResponseDto.setQuantity(userProductModel.getQuantity());
-        userProductResponseDto.setDescription(userProductModel.getDescription());
-        userProductResponseDto.setIsActive(userProductModel.getIsActive());
-        return userProductResponseDto;
-    }
-
-
     public UserProductResponseDto addUserProduct(UserProductRequest userProductRequest) {
         UserProductModel userProductModel = new UserProductModel();
         //TODO:: SHOULD I DO CHECKS FOR isPresent()??
@@ -77,8 +55,22 @@ public class UserService {
 
     }
 
+
 //    public UserProductResponseDto editUserProduct(UserProductRequest userProductRequest) {
 //
 //    }
+
+    public UserProductResponseDto setUserProductDto(UserProductModel userProductModel) {
+        UserProductResponseDto userProductResponseDto = new UserProductResponseDto();
+        userProductResponseDto.setId(userProductModel.getId());
+        userProductResponseDto.setUserId(userProductModel.getUser().getId());
+        userProductResponseDto.setProductId(userProductModel.getProduct().getId());
+        userProductResponseDto.setAmount(userProductModel.getAmount());
+        userProductResponseDto.setQuantity(userProductModel.getQuantity());
+        userProductResponseDto.setDescription(userProductModel.getDescription());
+        userProductResponseDto.setIsActive(userProductModel.getIsActive());
+        return userProductResponseDto;
+    }
+
 
 }
